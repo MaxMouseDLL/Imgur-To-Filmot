5fe859106865115e6e28cebd2b9dc6422813efd0
MaxMouseDLL <maxmousedll@gmail.com>
2015-12-04 19:32:45 +0000

    Minor Hacky updates to the core
    Gifv is blocked for me, so replace that with gif, https for some reason doesn't work, so force http, and weird things happen with i.filmot.org so I've removed that.

--- data/imgur-to-filmot.js
+++ data/imgur-to-filmot.js
@@ -1,6 +1,9 @@
 var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
 	if (links[i].href.indexOf("imgur.com") != -1) {
+	    links[i].href = links[i].href.replace("gifv","gif");
+	    links[i].href = links[i].href.replace("https","http");
+	    links[i].href = links[i].href.replace("i.imgur.com","filmot.org");
 	    links[i].href = links[i].href.replace("imgur.com","filmot.org");
 	}
 }
